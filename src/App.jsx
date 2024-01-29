bbfaf21b3ff9c85f06974d5b5f22926d67d2ec15
DineshGowthamK <dineshgowtham33@gmail.com>
2024-01-29 08:23:08 +0530

    Frontend

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Dashboard from './component/Dashboard'
+// export const API_URL = 'http://localhost:8000'
+export const API_URL = 'https://mongoose-xdhq.onrender.com'
+
+function App() {
+  return (
+  <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<Dashboard/>} />
+    </Routes>
+      </BrowserRouter>
+  )
+}
+
+export default App
